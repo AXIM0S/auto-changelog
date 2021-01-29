5295bcae70a13db7af85f80c41c783010f7a9e26
AXON <axonasif@gmail.com>
2021-01-29 15:55:22 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -29,14 +29,6 @@ async function run() {
     const changelog = await generate(octokit, exclude, owner, repo, tagRef);
 
     info(changelog);
-
-    const fs = require('fs');
-
-    // write to a new file named 2pac.txt
-    fs.writeFile('./CHANGELOG.md', changelog, (err) => {
-        // throws an error, you could also catch it here
-        if (err) throw err;
-    });
     
     setOutput("changelog", changelog);
   } catch (error) {
